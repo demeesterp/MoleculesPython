3fd0f8a017c163834e5b677916b835e29dc2b8fd
Piet Demeester <demeesterp@outlook.com>
2022-04-12 23:06:01 +0200

    read json files

--- Cmd/CmdMolAnalyse.py
+++ Cmd/CmdMolAnalyse.py
@@ -1,4 +1,24 @@
+from glob import glob
+import json
+from os import read
+from pathlib import Path, PurePath, PureWindowsPath
+
+
 class CmdMolAnalyse:
     def process(self, basePath):
-        print(basePath)
-        print("analyse")
+        files = self.ListMolecules(basePath)
+        newlist = []
+        for file in files:
+            f = open(file, "r")
+            fileData = f.read()            
+            newlist.append(json.loads(fileData))
+            f.close()
+
+        print(len(newlist))
+
+
+
+
+    def ListMolecules(self, basePath):        
+        path = Path(basePath)        
+        return list(path.glob('**/*.json'))
